4945af3330e88406e8b39b1b8a4451789732891d
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 00:28:31 +0900

    feat: Add ref and reactive

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,46 @@
 <script setup>
+import { ref, reactive } from 'vue'
+const title = ref('Vue.js Course')
+let price = ref(9)
+function increment() {
+  price.value += 1
+  instructor.age += 1
+  instructor.bio = 'hi'
+  instructor.sns.twitter = 'hello'
+  instructor.email = 'info@example.com'
+  info.value.students += 100
+}
+const info = ref({
+  students: 1000,
+  rating: 4,
+})
+const instructor = reactive({
+  name: 'Yoshipi',
+  age: 25,
+  sns: {
+    twitter: '@__yoshipi__',
+    youtube: '@yoshipi',
+  },
+  email: ref('yoshipi@example.com'),
+})
+instructor.bio = 'hello'
+const items = reactive([ref(1), ref(2), 3])
+items.reverse
+const courseInfo = {
+  sections: ref(10),
+  language: ref('Japanese'),
+}
 </script>
 
 <template>
+  <h1>{{ title }}</h1>
+  <h2>{{ price - 1 }}</h2>
+  <button @click="increment">button</button>
+  <h2>Students: {{ info.students }}</h2>
+  <h2>Instructor age: {{ instructor.age }}</h2>
+  <h2>Instructor bio: {{ instructor.bio }}</h2>
+  <h2>Instructor SNS Twitter: {{ instructor.sns.twitter }}</h2>
+  <h2>Instructor email: {{ instructor.email }}</h2>
+  <h2>Instructor email: {{ instructor.email }}</h2>
+  <h2>Course Section: {{ courseInfo.sections.value }}</h2>
 </template>
