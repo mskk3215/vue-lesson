2460181aad36f403d3ab205ec3c19f046d00d82e
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 14:52:49 +0900

    feat: Add template syntax

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <script setup>
+import { ref } from 'vue'
+// top levelで定義されているものだけtemplateに反映される
+const count = ref(2)
+const count2 = ref(4)
 </script>
 <template>
+  <!-- templateには単一の式のみかける -->
+  <div>{{ count + count2 }}</div>
+  <div>{{ count > 3 ? 'Yes' : 'No' }}</div>
 </template>
