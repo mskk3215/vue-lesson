d3730d9936ed75cc2a81bd9b1e8b1747a94adb5f
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 22:55:39 +0900

    refactor: Move child components in components directory

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import BaseIcon from './BaseIcon.vue'
+import BaseIcon from './components/BaseIcon.vue'
 
 const app = createApp(App)
 app.component('BaseIcon', BaseIcon) // 投入先のcomponentにimport文が記載されなくなるので保守性が悪くなるので頻出のものだけglobal登録する
