79e3ce65189608fefa4193535638b356167953a5
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 20:54:22 +0900

    feat:Add v-if directive

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,12 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { ref } from 'vue'
+const ok = ref(true)
+const maybeOk = ref(true)
+</script>
+<template>
+  <button @click="ok = !ok">toggle</button>
+  <!-- v-if,else-if,elseはまとめて書く -->
+  <p v-if="ok">OK!</p>
+  <p v-else-if="maybeOk">maybe OK!</p>
+  <p v-else>not OK!...</p>
+</template>
