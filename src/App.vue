aef20cdd673ae6ccc3a3c9b15a67cda2a8f46547
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 14:58:12 +0900

    feat: Add v-bind directive

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@ const count = ref(2)
 const count2 = ref(4)
 // v-htmlディレクティブ
 const message = ref('<h1>Password</h1>')
+
+// v-bindディレクティブ
+const vueURL = ref('https://vuejs.org')
 </script>
 <template>
   <!-- templateには単一の式のみかける -->
@@ -12,4 +15,6 @@ const message = ref('<h1>Password</h1>')
   <div>{{ count > 3 ? 'Yes' : 'No' }}</div>
   <!-- v-htmlディレクティブはhtmlを描画するためのもの -->
   <div v-html="message"></div>
+  <!-- v-bindディレクティブは要素の属性 (HTML 属性やカスタム属性) に動的な値を結び付けるために使用 -->
+  <a :id :href="vueURL" :foo>Vue.js</a>
 </template>
