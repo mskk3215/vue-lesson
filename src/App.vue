9d1765c71bb114d80f24f0f1ab9a01f3fdfb0ffe
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 21:59:45 +0900

    feat: Add v-for by numeric times

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,6 @@ const user = ref({
 </script>
 <template>
   <p v-for="(value, key, index) in user" :key="value">{{ key }}:{{ value }}({{ index }})</p>
+  <p v-for="n in 10" :key="n">{{ n }}</p>
+  <p v-for="n of 10" :key="n">{{ n }}</p>
 </template>
