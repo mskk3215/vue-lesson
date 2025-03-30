00ba6aaeb6da5976f9e65bc40280bc10824f57fe
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 14:55:13 +0900

    feat: Add v-html directive

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,13 @@ import { ref } from 'vue'
 // top levelで定義されているものだけtemplateに反映される
 const count = ref(2)
 const count2 = ref(4)
+// v-htmlディレクティブ
+const message = ref('<h1>Password</h1>')
 </script>
 <template>
   <!-- templateには単一の式のみかける -->
   <div>{{ count + count2 }}</div>
   <div>{{ count > 3 ? 'Yes' : 'No' }}</div>
+  <!-- v-htmlディレクティブはhtmlを描画するためのもの -->
+  <div v-html="message"></div>
 </template>
