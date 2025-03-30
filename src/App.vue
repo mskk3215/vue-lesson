be190e14f151875c7460f807c1c6cc02a37931bc
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 21:36:38 +0900

    feat: Add v-for directive

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
 import { ref } from 'vue'
-const ok = ref(true)
+const fruits = ref([
+  { id: 1, name: 'Apple' },
+  { id: 2, name: 'Banana' },
+  { id: 3, name: 'Grape' },
+  { id: 4, name: 'Orange' },
+])
 </script>
 <template>
-  <button @click="ok = !ok">toggle</button>
-  <!-- 高頻度で切り替えるものはv-showを使用する -->
-  <p v-show="ok">OK!</p>
+  <!-- v-forの時はkeyが必要。v-ifは自動で付与してくれる。 -->
+  <!-- indexとループのcontentは一致しないのでkeyにindexを使用しない -->
+  <!-- v-forでは分割代入ができる -->
+  <li v-for="({ id, name }, index) in fruits" :key="id">{{ name }}({{ index }})</li>
 </template>
