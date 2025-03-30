988b650952c3ac4a1a5c5bd0c9617dcdaf803738
mskk3215 <m.kaaiweb@gmail.com>
2025-03-31 00:03:54 +0900

    feat: Add style with global css

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import BaseIcon from './components/BaseIcon.vue'
+import './assets/main.css'
 
 const app = createApp(App)
 app.component('BaseIcon', BaseIcon) // 投入先のcomponentにimport文が記載されなくなるので保守性が悪くなるので頻出のものだけglobal登録する
