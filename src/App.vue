3f2a3b730cb9873d9de9b7c8a684d78c22f2948d
mskk3215 <m.kaaiweb@gmail.com>
2025-03-30 21:18:58 +0900

    feat: Add v-show directive

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 const ok = ref(true)
-const maybeOk = ref(true)
 </script>
 <template>
   <button @click="ok = !ok">toggle</button>
-  <!-- v-if,else-if,elseはまとめて書く -->
-  <p v-if="ok">OK!</p>
-  <p v-else-if="maybeOk">maybe OK!</p>
-  <p v-else>not OK!...</p>
-  <!-- 同じ条件であればtemplateでまとめてかける -->
-  <template v-if="ok">
-    <p>OK!</p>
-    <p>Hello!</p>
-    <p>Hi!</p>
-  </template>
+  <!-- 高頻度で切り替えるものはv-showを使用する -->
+  <p v-show="ok">OK!</p>
 </template>
