41919715bac664e87c656cd0611cc7e9a258936e
mskk3215 <m.kaaiweb@gmail.com>
2025-04-06 23:44:09 +0900

    feat: Add props passing from parent to children

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import CountUp from './components/CountUp.vue'
 import BaseButton from './components/BaseButton.vue'
+import { ref } from 'vue'
+import CountDisplay from './components/CountDisplay.vue'
+const count = ref(0)
 </script>
 <template>
   <h1 class="red">App</h1>
   <BaseIcon />
   <CountUp />
   <BaseButton id="base-button" class="border" @click="console.log('App.vue')" />
+  <CountDisplay :count-props="count" bar="bar" />
+  <button @click="count++">+1</button>
 </template>
 <!-- scopedをつけてcomponent内だけで使用するのが一般的 -->
 <style scoped>
---
+++ src/components/CountDisplay.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  countProps: {
+    type: [Number, String],
+    required: true,
+    // default: 1,
+  },
+})
+</script>
+<template>
+  <p>count: {{ count }}</p>
+</template>
