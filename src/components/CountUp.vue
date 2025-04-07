9ab1f5f3ee24098472518bcd532bdb45de54cd9b
mskk3215 <m.kaaiweb@gmail.com>
2025-04-08 00:48:33 +0900

    feat: Add component lifecycle functions

--- src/components/CountUp.vue
+++ src/components/CountUp.vue
@@ -1,6 +1,39 @@
 <script setup>
-import { ref } from 'vue'
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+  nextTick,
+} from 'vue'
 const count = ref(9)
+onBeforeMount(() => {
+  console.log('onBeforeMount')
+})
+onMounted(() => {
+  console.log('onMounted')
+})
+onBeforeUpdate(() => {
+  console.log('onBeforeUpdate')
+})
+onUpdated(() => {
+  console.log('onUpdated')
+})
+
+onBeforeUnmount(() => {
+  console.log('onBeforeUnmount')
+})
+onUnmounted(() => {
+  console.log('onUnmounted')
+})
+// Domの更新後に実行される
+nextTick(() => {
+  count.value++
+  console.log('nextTick')
+})
 </script>
 <template>
   <h2 class="red">CountUp</h2>
