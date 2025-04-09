ee10a687ca53055e805beeaf0d956c1206f0d312
mskk3215 <m.kaaiweb@gmail.com>
2025-04-09 23:40:23 +0900

    chore: Remove unnecessary codes

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,4 @@
 <script setup>
-import { ref } from 'vue'
-import CountDisplay from './components/CountDisplay.vue'
-import ResetButton from './components/ResetButton.vue'
-const count = ref(0)
-function onReset(value) {
-  count.value = value
-}
 </script>
 <template>
-  <CountDisplay :count-props="count" bar="bar" />
-  <button @click="count++">+1</button>
-  <ResetButton @reset="count = 0" />
-  <ResetButton @reset-count="onReset" />
 </template>
