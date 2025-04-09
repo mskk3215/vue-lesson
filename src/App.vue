7a39d26c9d65bbe7ee9923a43c33af876c59533b
mskk3215 <m.kaaiweb@gmail.com>
2025-04-09 23:46:50 +0900

    feat: Add slot syntax

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <script setup>
+import BaseCard from './components/BaseCard.vue'
 </script>
 <template>
+  <BaseCard>
+    <h1>Hello</h1>
+    <p>How are you?</p></BaseCard
+  >
 </template>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+<style scoped>
+div {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  border-radius: 10px;
+  padding: 1rem;
+}
+</style>
